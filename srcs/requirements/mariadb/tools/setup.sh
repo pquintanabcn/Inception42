9a189648d8a7e322dea3a6268c3eda8a45f25e2c
Patricia Quintana barcelona <pquintan@cbr14s6.42barcelona.com>
2025-02-07 14:37:00 +0100

    little changes on setup mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -2,10 +2,18 @@
 
 echo "Iniciando MariaDB..."
 mariadb-install-db --user=mysql --ldata=/var/lib/mysql
-mysqld_safe --datadir='/var/lib/mysql' &
+
 
 # Wait to iniciate
-sleep 5
+mysqld_safe --datadir='/var/lib/mysql' &
+sleep 10
+
+while ! mariadb -u root -e "SELECT 1" >/dev/null 2>&1; do
+	echo "Waiting for Mariadb to be ready..."
+	sleep 2
+done
+
+echo "MariaDB ready, executing initial conf..."
 
 # maybe change something
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
@@ -19,28 +27,3 @@ mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
 # Iniciar MariaDB en primer plano
 exec mysqld_safe --datadir='/var/lib/mysql'
-
-
-#echo "Waiting for MariaDB to start..."
-#until mysqladmin ping --silent; do
-#    sleep 1
-#done
-
-# Crear base de datos si no existe
-#if ! mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "USE ${MYSQL_DATABASE}"; then
-#    echo "Creating database ${MYSQL_DATABASE}..."
-#    mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE};"
-#fi
-
-# Crear usuario y asignar privilegios
-#echo "Creating user ${MYSQL_USER}..."
-#mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-#mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-#mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-
-# Asegurarse de que el usuario root tenga la contraseña configurada
-#echo "Setting root password..."
-#mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
-#echo "MariaDB setup complete."
-
